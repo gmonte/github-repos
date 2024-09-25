237603a72cd31f9d65a823eb56440656dec292c1
Guilherme Monte <gmonte@inf.ufsm.br>
2024-09-25 20:55:42 -0300

    refactor(custom): clean up

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,13 +2,6 @@
 
 interface ImportMetaEnv {
   readonly VITE_GITHUB_TOKEN: string
-
-  // readonly VITE_KEYCLOAK_BASE_URL: string
-  // readonly VITE_KEYCLOAK_REALM: string
-  // readonly VITE_KEYCLOAK_CLIENT_ID: string
-  // readonly VITE_KEYCLOAK_CLIENT_SECRET: string
-
-  // more env variables...
 }
 
 interface ImportMeta {
